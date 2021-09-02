6f1fde1dbc7f35cc7201c7c7cc8fba11f83d3f03
Maximilon Baddeley <maximilon@Maximilons-iMac.local>
2021-09-03 11:40:14 +1200

    add request

--- client/apis/News.js
+++ client/apis/News.js
@@ -1,46 +1,36 @@
 import request from 'superagent'
 
-const url = 'https://api.thenewsapi.com/v1/news/top?api_token=ASxRg3pkGB5dMPv7ALh6lpzPxMGgxm8ePBrM84wE&language=en&categories='
+const url =
+  'https://api.thenewsapi.com/v1/news/top?api_token=ASxRg3pkGB5dMPv7ALh6lpzPxMGgxm8ePBrM84wE&language=en&categories='
 
 export const getNews = () => {
-    .get(`${url}entertainment`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}entertainment`).then((response) => {
+    return response.body
+  })
 }
 
 export const getFoodNews = () => {
-  return request
-    .get(`${url}food`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}food`).then((response) => {
+    return response.body
+  })
 }
 export const getTechNews = () => {
-  return request
-    .get(`${url}tech`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}tech`).then((response) => {
+    return response.body
+  })
 }
 export const getHealthNews = () => {
-  return request
-    .get(`${url}health`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}health`).then((response) => {
+    return response.body
+  })
 }
 export const getBusinessNews = () => {
-  return request
-    .get(`${url}business`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}business`).then((response) => {
+    return response.body
+  })
 }
 export const getPoliticsNews = () => {
-  return request
-    .get(`${url}politics`)
-    .then(response => {
-      return response.body
-    })
+  return request.get(`${url}politics`).then((response) => {
+    return response.body
+  })
 }
