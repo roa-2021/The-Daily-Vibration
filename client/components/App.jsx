f345cd4cf3232f309601e5bd40fbe83ddd6be617
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-02 21:50:29 +1200

    add crests

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,11 +3,11 @@ import { Route } from 'react-router'
 import News from './News'
 import Forms from './Forms'
 
-
 const App = () => {
+  const slytherinCrest = <img className="slytherin-crest" src='/images/slytherin_crest.png' />
   return (
     <>
-      <h1>The Daily Vibration</h1>
+      <h1>{slytherinCrest}The Daily Vibration{slytherinCrest}</h1>
       <Forms />
       <Route path='/' component={News}/>
 
