b8cf267c8be328babfe84a81f0516bd7a1ecf625
Psalms91-50-23 <khothach@gmail.com>
2021-09-02 19:57:39 +1200

    finish form component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import News from './News'
+import Forms from './Forms'
+
 
 const App = () => {
   return (
     <>
       <h1>The Daily Vibration</h1>
       <News />
+      <Forms />
     </>
   )
 }
