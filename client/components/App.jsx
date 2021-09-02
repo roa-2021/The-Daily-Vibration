32789c1143555ab898665d8b0bf2d087c2d5c53e
phill-mutu <7622261+Lysias91@users.noreply.github.com>
2021-09-02 22:11:14 +1200

    add crests as img tags

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,13 +4,17 @@ import News from './News'
 import Forms from './Forms'
 
 const App = () => {
-  const slytherinCrest = <img className="slytherin-crest" src='/images/slytherin_crest.png' />
   return (
     <>
-      <h1>{slytherinCrest}The Daily Vibration{slytherinCrest}</h1>
+      <header>
+        <div>
+          <img className='slytherin-crest' src='/images/slytherin_crest.png' />
+          <h1>The Daily Vibration</h1>
+          <img className='slytherin-crest' src='/images/slytherin_crest.png' />
+        </div>
+      </header>
       <Forms />
-      <Route path='/' component={News}/>
-
+      <Route path='/' component={News} />
     </>
   )
 }
